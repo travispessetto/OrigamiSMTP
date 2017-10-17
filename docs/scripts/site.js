444661a7efbae323ccaa6106649980d40b274ab9
Travis Pessetto <travis@pessetto.com>
2017-10-17 15:45:08 -0600

    Some updates to the website

--- docs/scripts/site.js
+++ docs/scripts/site.js
@@ -1,3 +1,9 @@
+// global stuff
+var lastScrollTop = 0;
+var scrollPage = 0;
+var canScroll = true;
+
+// angular stuff
 var app = angular.module('origamiDownloads',[]);
 app.controller('origamiController', function($scope,$http,$sce){
   $scope.method = "JSONP";
@@ -6,10 +12,102 @@ app.controller('origamiController', function($scope,$http,$sce){
   	.then(function(response)
   	{
   		$scope.status = status;
-  		console.log(status);
   		$scope.versions = response.data.data;
   	},
   	function(response) {
        console.error(response);
   	});
 });
+
+// jquery stuff
+$(document).ready(function()
+{
+	$("a").on('click',scrollToAnchor);
+	$(document).on('scroll',scrollToNextPage);
+	$(window).resize(function()
+	{
+		lastScrollTop = $(document).scrollTop();
+		centerContainers();
+	});
+	centerContainers();
+});
+
+var scrollToAnchor = function(event)
+{
+	if(canScroll)
+	{
+		event.preventDefault();
+		var hash = this.hash;
+		var pages = $(".fullpage");
+		var index = 0;
+		pages.each(function()
+		{
+			if("#"+$(this).attr('id') == hash)
+			{
+				scrollToLocation($(this).offset().top);
+				scrollPage = index;
+			}
+			++index;
+		});
+	}
+}
+
+var scrollToNextPage = function()
+{
+	if(canScroll)
+	{
+		var st = $(document).scrollTop();
+		var down = st > lastScrollTop;
+		var up = st < lastScrollTop;
+		var pages = $(document).find('.fullpage');
+		if(down)
+		{
+			++scrollPage;
+		}
+		if(up)
+		{
+			--scrollPage;
+		}
+		if(scrollPage < 0)
+		{
+			scrollPage = 0;
+		}
+		if(scrollPage >= pages.length)
+		{
+			scrollPage = pages.length - 1;
+		}
+		var page = pages[scrollPage];
+		console.log("scroll page is : " + scrollPage);
+		var locationTop = $(page).offset().top;
+		scrollToLocation(locationTop);
+	}
+}
+
+
+var scrollToLocation = function(locationTop)
+{
+	if(canScroll)
+	{
+		console.log("perform scroll event");
+		canScroll = false;
+		$('html, body').animate({scrollTop: locationTop}).promise().done(function()
+		{
+			lastScrollTop = $(document).scrollTop();
+			canScroll = true;
+			$('html, body').css('overflow','visible');
+			console.log("scroll event complete");
+		});
+	}
+}
+
+var centerContainers = function()
+{
+	$('.center-container').each(function()
+	{
+		var page = $(this).closest('.fullpage');
+		var pageHeight = page.height();
+		var contentHeight = $(this).height();
+		var newPadding = (pageHeight - contentHeight) / 2;
+		$(this).css('padding-top',newPadding+"px");
+	});
+}
