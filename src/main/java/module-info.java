41bd1fdaa0f18774480cee3f87609d4ada3b1666
Travis Pessetto <travis@pessetto.com>
2022-04-20 22:16:37 -0600

    modularize and update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module OrigamiSMTP {
+    requires info.picocli;
+    requires mail;
+    requires javafx.base;
+    
+    exports com.pessetto.origamismtp;
+}
